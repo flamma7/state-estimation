dbfc400fe3754e5552f8d64279e28d0530fd8ae3
Luke Barbier <lukebarbier12@gmail.com>
2019-08-19 10:51:34 -0600

    cleaned up comments and printing output

--- least_squares/unweighted_least_sq.py
+++ least_squares/unweighted_least_sq.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
+"""
+We have a single constant number, take 1000 measurements w/ gaussian noise added
+Estimate via unweighted least squares the constant number
+Print out the error percentage
+"""
 
 import numpy as np
 
-"""
-Basically I have a single constant number
-Add gaussian noise to it
-Run linear least squares on it, and see what we get
-"""
 
-# get random number and print
-# get a vector of 50 random samples of the number
-# run least squares on it and see how close we get
 num_meas = 1000
-
 rand_const = np.random.randint(0,100)
-print rand_const
 
 measurements = np.random.normal(rand_const, 10, size=(num_meas,1))
 ones = np.ones((num_meas, 1))
 hl = np.linalg.pinv(ones)
-estimate = np.dot(hl, measurements)
-print estimate
+estimate = np.dot(hl, measurements).item()
+error = ( ( estimate-rand_const ) / rand_const )
+
+print("Original Number: %d" % rand_const)
+print("Estimate: %f" % estimate)
+print("Error percentage: "+"{:.2%}".format(abs(error)))
--- least_squares/weighted_least_sq.py
+++ least_squares/weighted_least_sq.py
@@ -5,10 +5,11 @@ from numpy.linalg import pinv
 from numpy import dot
 
 """
-Single constant number
+We have a single constant number
 We have one very noisy measurement source called "bad"
-One not so noise measurement source called "good"
-Run weighted least squares to estimate the constant
+One more accurate measurement source called "good"
+Estimate via weighted least squares the constant number
+Print out the error percentage
 """
 
 num_meas_total = 20
@@ -16,7 +17,6 @@ bad_sigma = 10
 good_sigma = 2
 
 rand_const = np.random.randint(0,100)
-print rand_const
 
 num_bad_meas = int(num_meas_total / 2)
 # num_bad_meas = num_meas_total -1 # almost all bad measurements
@@ -25,7 +25,6 @@ bad_measurements = np.random.normal(rand_const, bad_sigma, size=(num_bad_meas,1)
 # print(bad_measurements)
 good_measurements = np.random.normal(rand_const, good_sigma, size=(num_good_meas,1))
 measurements = np.concatenate(( bad_measurements, good_measurements))
-print(measurements)
 
 H = np.ones((num_meas_total, 1))
 bad_meas_covariances_vector = np.ones((num_bad_meas,1)) * ( bad_sigma ** 2)
@@ -40,5 +39,10 @@ R = meas_covariance
 estimate = pinv( dot( dot( H.T, pinv(R)), H))
 estimate = dot( estimate, H.T)
 estimate = dot( estimate, pinv(R))
-estimate = dot( estimate, measurements)
-print( estimate )
+estimate = dot( estimate, measurements).item()
+error = ( ( estimate-rand_const ) / rand_const )
+
+print("Original Number: %d" % rand_const)
+print("Measurements: \n"+str(measurements))
+print("Estimate: %f" % estimate)
+print("Error percentage: "+"{:.2%}".format(abs(error)))
