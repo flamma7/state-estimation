0a3cefc5ba00678807d0e6b1a64322eb91a8d064
Luke <lukebarbier12@gmail.com>
2019-11-16 11:50:19 -0700

    rk45 syntax figured out

---
+++ turtlesim/filters/test_rk45.py
@@ -0,0 +1,20 @@
+from __future__ import division
+from scipy import integrate
+import numpy as np
+
+# simple example: dydx = x --> y = (1/2)*x^2
+
+def diff_eq(x, y):
+    print("calculating... " + str(y))
+    return np.array([x])
+
+x0, x1 = 0, 5
+y0 = np.array([0])
+
+r = integrate.RK45(diff_eq, x0, y0, x1)
+while r.status == "running":
+    status = r.step()
+print(r.y)
+
+# plt.show(y,x_seq)
+# plt.show()
